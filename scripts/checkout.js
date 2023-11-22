18be4e45e822cff1df6a5deb22b838e4b2b35536
ABHAY GUPTA <105373537+AMAY369@users.noreply.github.com>
2023-11-22 10:26:16 +0530

    update ui when clicking on delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -12,7 +12,7 @@ cart.forEach((cartItem)=>{
     console.log(matchingItems);
     cartSummaryHTML+=
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingItems.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -90,10 +90,13 @@ cart.forEach((cartItem)=>{
     `
     document.querySelector('.order-summary').innerHTML = cartSummaryHTML;
 
-    document.querySelectorAll('.js-delete-quantity-link').forEach((event)=>{
-      event.addEventListener('click',()=>{
-        const productId = event.dataset.productId;
+    document.querySelectorAll('.js-delete-quantity-link').forEach((link)=>{
+      link.addEventListener('click',()=>{
+        const productId = link.dataset.productId;
         removeFromCart(productId);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
       });
     })
 });
