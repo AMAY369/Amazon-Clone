ad2f66e9377341fecac182bf869c9692d3d6e6cb
ABHAY GUPTA <105373537+AMAY369@users.noreply.github.com>
2023-11-23 20:54:38 +0530

    used external library dayjs

--- data/cart.js
+++ data/cart.js
@@ -3,11 +3,13 @@ export let cart = JSON.parse(localStorage.getItem('cart'))
     [
         {
             productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity:2
+            quantity:2,
+            deliveryOptionId:'1'
         },
         {
             productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 1
+            quantity: 1,
+            deliveryOptionId:'2'
         }
     ];
 
@@ -40,14 +42,15 @@ export function addToCart(productId, itemQuantity){
               cart.push(
                   {
                       productId: productId,
-                      quantity: (Number) (itemQuantity)
+                      quantity: (Number) (itemQuantity),
+                      deliveryOptionId:'1'
                   }
               );
           }
           saveToStorage();
   }
 
-  export function removeFromCart(productId){
+export function removeFromCart(productId){
     const newCart = [];
     cart.forEach((cartItem)=>{
         if(cartItem.productId!==productId){
@@ -57,3 +60,12 @@ export function addToCart(productId, itemQuantity){
     cart = newCart;
     saveToStorage();
   }
+
+export function updateQuantity(productId, newQuantity){
+    cart.forEach((item)=>{
+        if(item.productId === productId){
+            item.quantity = newQuantity;
+        }
+        saveToStorage();
+    });
+}
