179262f3de048c4f3aad7c90057ad39839a1c79d
ABHAY <369abhayg@gmail.com>
2024-05-01 15:16:50 +0530

    generate html for cart summary

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product)=>{
   </div>
 
   <div class="product-price">
-  &#8377;${product.priceCents}
+  &#8377;${product.price}
   </div>
 
   <div class="product-quantity-container">
